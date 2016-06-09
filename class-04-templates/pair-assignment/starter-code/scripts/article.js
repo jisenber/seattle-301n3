278a194aa4fd24420ab50328492fe406e3065231
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-06-08 20:00:41 -0700

    lab 04 complete

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -10,6 +10,9 @@ function Article(opts) {
 }
 
 Article.prototype.toHtml = function() {
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
@@ -19,20 +22,22 @@ Article.prototype.toHtml = function() {
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-  var context = {
-    data: rawData
-  };
   var source = $('#article-template').html();
+  var context = {
+    data: rawData,
+    daysAgo: this.daysAgo,
+    publishStatus: this.publishStatus
+  };
+  console.log(context.data);
   var template = Handlebars.compile(source);
-  $(document.body).append(template);
-  Handlebars.registerHelper(function() {
-    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
-    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-    rawData.sort(function(a, b) {
-      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-    });
-  });
+  var store = template(context);
+  console.log('me too!');
+  return store;
 };
+
+rawData.sort(function(a, b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
@@ -40,4 +45,5 @@ rawData.forEach(function(ele) {
 
 articles.forEach(function(a) {
   $('#articles').append(a.toHtml());
+  console.log('works!');
 });

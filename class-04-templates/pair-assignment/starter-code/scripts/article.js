92784c6b390a23372b0bed76b9ce25430561c3d1
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-06-08 19:06:29 -0700

    code n stuff

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -1,6 +1,6 @@
 var articles = [];
 
-function Article (opts) {
+function Article(opts) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
   this.title = opts.title;
@@ -18,20 +18,26 @@ Article.prototype.toHtml = function() {
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  var context = {
+    data: rawData
+  };
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
+  $(document.body).append(template);
+  Handlebars.registerHelper(function() {
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    rawData.sort(function(a, b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+  });
 };
-
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
+});
 
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+articles.forEach(function(a) {
+  $('#articles').append(a.toHtml());
 });

87400d584ae80acfdbfbb59ebd5b445a8d4eccb3
jisenber <isenberg.jacob@yahoo.com>
2016-07-03 09:38:50 -0700

    1st commit to 11th pair assignment

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -3,8 +3,11 @@
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
-
+    $('.icon-address-book').on('click', function() {
+      $('#articles').hide();
+      $('#about').show();
+    });
   };
-
+  
   module.aboutController = aboutController;
 })(window);

dac38acf3219b430c2da09ad03196851b16225cf
Nassir Isaf <njisaf@gmail.com>
2016-07-10 16:15:06 -0700

    articlesController.loadAll

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -38,6 +38,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // Jacob+Nassir: .loadAll is invoked on routes.js when the homepage (root) is loaded. This function is using page.js. This function checks if there are objects in the Article.all array, by confirming if it has length. If it does have length, it assumes that an JSON data pull has already occurred to populate Article.all, and it adds this array to the ctx object as a property, "articles". If the Article.all array has no length, Article.fetchAll gets the data. .fetchAll takes an argument of articleData, which is a variable scoped to this function only. articleData attaches the newly-populated Article.all array to the ctx object. In either case, the next function is invoked, which in this case is articlesController.index. tl;dr -- this function takes the Article objects and passes them to articlesController.index as part of the ctx object. 
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
